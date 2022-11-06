1b3e4ab745cfb2f9e643e3f83962a3d920144b31
kavithaclothing <117513764+kavithaclothing@users.noreply.github.com>
2022-11-06 18:03:35 +0530

    ok

---
+++ client/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us</h5>
+            <p>+91 9745466081</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Location</h5>
+            <p>
+            Nettoor
+            </p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-office"></i>
+            </div>
+            <h5>Landline</h5>
+            <p>0484 4858478</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
